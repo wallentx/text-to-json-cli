578e231efb8744c801db26f839e6f02d6ce2638b
Max <me@maxlloyd.no>
2015-11-22 02:16:42 +0100

    Initial Commit

---
+++ bin/ttj.js
@@ -0,0 +1,75 @@
+#!/usr/bin/env node
+
+"use strict";
+
+const fs = require('fs');
+const chalk = require('chalk');
+const meow = require('meow');
+const text_to_json = require('plain-text-data-to-json');
+
+const cli = meow(`
++---------------------------------------------------------------------+
+|  _____ _____ __ __ _____    _____ _____       __ _____ _____ _____  |
+| |_   _|   __|  |  |_   _|  |_   _|     |   __|  |   __|     |   | | |
+|   | | |   __|-   -| | |      | | |  |  |  |  |  |__   |  |  | | | | |
+|   |_| |_____|__|__| |_|      |_| |_____|  |_____|_____|_____|_|___| |
+|                                                                     |
+|    Usage                                                            |
+|      $ ttj <file>                                                   |
+|                                                                     |
+|    Options                                                          |
+|      -f,  --file,  File to convert                                  |
+|      -c,  --comments, allow comments                                |
+|      -d,  --delimiter, delimiter for key value pairs                |
+|      -fg, --forgiving, doesn't throw error for duplicate keys       |
+|      -l,  --log, Wether to log when forgiving ignores an error      |
+|                                                                     |
+|    Examples                                                         |
+|      $ ttj --file unicorns.txt                                      |
+|                                                                     |
++---------------------------------------------------------------------+
+`, {
+    alias: {
+        r: 'rainbow'
+    }
+});
+
+
+// Process commands
+if(cli.input[0] === 'help') {
+  console.log(cli.help);
+}
+
+
+
+if(cli.flags.f) {
+  let options = {};
+  options.file       = cli.flags.file      || cli.flags.f;
+  options.comments   = cli.flags.comments  || cli.flags.c   || '%';
+  options.delimiter  = cli.flags.delimiter || cli.flags.d   || ':';
+  options.forgiving  = cli.flags.forgiving || cli.flags.fg  || false;
+  options.log        = cli.flags.log       || cli.flags.l   || true;
+  options.output     = cli.flags.output    || cli.flags.o   || options.file;
+
+  fs.readFile(options.file, (err, data) => {
+    if(err) {throw err;}
+    let filtered   = text_to_json(data.toString(), options);
+    options.output = options.output.replace(/\.[\w]+$/g, '.json');
+
+    console.log(options.output);
+
+    fs.writeFile(options.output, JSON.stringify(filtered), (err) => {
+      if (err) {throw err;}
+      console.log(chalk.green('Hurra, finished with any problems. Good luck on your project! \n'));
+    });
+  });
+} else {
+  cli.showHelp();
+}
+
+
+
+
+ process.on('uncaughtException', (err) => {
+   console.log(err);
+ });
---
+++ read.md
@@ -0,0 +1,56 @@
+![Logo](http://i.imgur.com/HzEq0fj.jpg?1)
+
+## Install
+```
+$ npm install -g text-to-json-cli
+```
+
+
+## 🌈 Usage 🌈
+```
+
+$ ttj <file>
+
+Options                                                          
+     -f,  --file,  File to convert                                  
+     -c,  --comments, allow comments                                
+     -d,  --delimiter, delimiter for key value pairs                
+     -fg, --forgiving, doesn't throw error for duplicate keys (recommend leaving this off )     -l,  --log, whether to log when forgiving ignores an error
+
+
+Examples
+  $ ttj --file "unicorn.txt" --output "super-unicorn.json"
+  $ ttj --file "unicorn.txt" --output "super-unicorn.json" --forgiving
+  $ ttj --file "unicorn.txt" --delimiter "="
+```
+
+## 🍭 API 🍭
+### ttj
+##### --file
+Type: ``` string ```
+
+File to be processed
+
+##### --comments
+Type: ``` string ```
+
+This is the character that will be used to signify comments. Default is "%"
+
+#### --delimiter
+Type: ``` string ```
+
+This is the character that will be used to seperate key/value pairs. Default is ":"
+
+#### --forgiving
+Type: ``` boolean ```
+
+Adding this will allow the existence of duplicate keys and not throw an error. This is false by default.
+
+#### --log
+
+Adding this will log when forgiving ignores an error. This is true by default.
+
+
+
+## License
+MIT ©[Maximilian Lloyd](http://www.maxlloyd.no)
